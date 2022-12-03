8c836b40efc5481c306b122f76c3d58ea6832b39
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-12-03 22:12:01 +0300

    Create has through relationship

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
-  resources :cars do
-    resources :reservations
-  end
+  resources :cars
+  resources :reservations
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
